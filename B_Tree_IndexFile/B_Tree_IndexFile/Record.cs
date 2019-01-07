3bf89223bbbab9de0c8d5322397f2b039991f991
Pridz <koparka.p@gmail.com>
2019-01-07 22:44:41 +0100

    New class. Represents record data from file.

---
+++ B_Tree_IndexFile/B_Tree_IndexFile/Record.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace B_Tree_IndexFile
+{
+    class Record
+    {
+        char[] name { get; set; } 
+        char[] surname { get; set; }
+        ulong PESEL { get; set; }
+        uint key { get; set; }
+        ulong position { get; set; }
+
+        public Record()
+        {
+            name = null;
+            surname = null;
+            PESEL = 0;
+            key = 0;
+            position = 0;
+        }
+
+        bool isNull()
+        {
+            bool state = true;
+            if (name != null)
+            {
+                return false;
+            }
+            if (surname != null)
+            {
+                return false;
+            }
+            if (PESEL != 0)
+            {
+                return false;
+            }
+            if (key != 0)
+            {
+                return false;
+            }
+            if (position != 0)
+            {
+                return false;
+            }
+            return state;
+        }
+
+        uint calculateKeyValue()
+        {
+            return KeyValueCalculator.calculateNameValue(name) +
+                    KeyValueCalculator.calculateSurnameValue(surname) +
+                    KeyValueCalculator.calculatePeselValue(PESEL);
+        }
+    }
+}
